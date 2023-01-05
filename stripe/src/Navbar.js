ae0002ee1c6ed3d8fb0ddd51092ee47a32f7f1eb
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2023-01-05 16:38:45 +0530

    usecontext adv

---
+++ stripe/src/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
+//function to show the navbar
+const Navbar = () => {
+  // using state from global context(custom hook)
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  // display submenu based on event, get the metrics for event , display the submenu in middle of the category,3px from bottom
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+  // only display submenu when hovering over link-btn
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu();
+    }
+  };
+  return (
+    <nav className='nav' onMouseOver={handleSubmenu}>
+      <div className='nav-center'>
+        {/* in case of small screens */}
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Sign in</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
